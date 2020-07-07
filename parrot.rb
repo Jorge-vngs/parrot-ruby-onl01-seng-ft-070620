c47e709eb99cbee85cd6795ea4aa41662aaa9d75
Jorge Venegas Martinez <brangelina01@gmail.com>
2020-07-07 00:39:51 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,19 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot
+  parrot_type =  "Pretty, Bird!"
+  puts "Squawk!"
+  return "Squawk!"
+  
+end 
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  # Create method `parrot` that outputs a given phrase and
 # returns the phrase
